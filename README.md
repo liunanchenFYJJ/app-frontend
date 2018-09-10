273be106d1955d2ca996a811a456137e0b8ec4b1
liunanchenFYJJ <liunan_chen@163.com>
2018-09-10 09:25:05 +0800

    init project

---
+++ README.md
@@ -0,0 +1,23 @@
+# app-frontend
+
+> An app-frontend project based on Vue.js && vux-ui
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm start
+
+# test with standard
+npm test
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+[![JavaScript Style Guide](https://cdn.rawgit.com/standard/standard/master/badge.svg)](https://github.com/standard/standard)
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <van-button type="primary" @click="query">request</van-button>
+  </div>
+</template>
+
+<script>
+import $ from "jquery";
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: ''
+    }
+  },
+  methods: {
+    query: function() {
+      let self = this;
+      self.$axios.get('/api/v2/movie/top250')
+        .then(function (response) {
+          self.msg = response.data.title;
+        })
+        .catch(function (response) {
+          console.log(response);
+        });
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
