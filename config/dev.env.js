ef9c8c122be0fcfb4d564aa149799f9f8a2b40d5
liunanchenFYJJ <liunan_chen@163.com>
2018-09-18 22:56:30 +0800

    change api

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_ROOT: '"//192.168.0.119:8080/power/"' // 开发接口
+  // API_ROOT: '"//192.168.0.119:8080/power/"' // 开发接口
+  API_ROOT: '"//power.szunipower.com.cn/"' // 开发接口
 })
