c59ac9da65c7bcfc46bdd03222a5590fc0a33af7
liunanchenFYJJ <liunan_chen@163.com>
2018-11-15 15:51:18 +0800

    add typescript

---
+++ src/typescript/demo.js
@@ -0,0 +1,5 @@
+function sayHello(person) {
+    return "hello, " + person;
+}
+var user = 'jj';
+console.log(sayHello(user));
---
+++ src/typescript/demo.ts
@@ -0,0 +1,6 @@
+function sayHello(person: string) {
+  return `hello, ${person}`;
+}
+
+let user = 'jj';
+console.log(sayHello(user));
